7148ec6b7ed64bc804b6be0e098957567ced1935
‘mxg133’ <396330646@qq.com>
2020-10-10 15:57:32 +0800

    2020-10-10

---
+++ 06_servlet/src/Hellosservlet.java
@@ -0,0 +1,60 @@
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author 孟享广
+ * @date 2020-10-10 10:07 上午
+ * @description
+ */
+public class Hellosservlet implements Servlet {
+    //单例的 就创建一次
+    public Hellosservlet() {
+        System.out.println("1 Hellosservlet的构造器····");
+    }
+
+    //单例的 就创建一次
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("2 Hellosservlet的init()初始化方法····");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    //每次访问 都会调用
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("3 Hellosservlet的service()方法····");
+        //类型转换
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        //获取请求的方式
+        String method = httpServletRequest.getMethod();
+//        System.out.println(method);
+        if ("GET".equals(method)) {
+            doGet();
+        }else if ("POST".equals(method)){
+            doPost();
+        }
+    }
+
+    public void doGet() {
+        System.out.println("get请求····");
+    }
+
+    public void doPost() {
+        System.out.println("post请求····");
+    }
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    //停止的时候会被调用
+    @Override
+    public void destroy() {
+        System.out.println("4 Hellosservlet的destroy()销毁方法····");
+    }
+}
---
+++ 06_servlet/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mxg
+  Date: 2020/10/10
+  Time: 11:06 上午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$2322211111111
+  </body>
+</html>
